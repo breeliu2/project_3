570bcfdf6291109287f59a082c253e0216258981
asujacv-nsYiXe <125230352+asujacv-nsYiXe@users.noreply.github.com>
2023-06-26 21:18:01 -0500

    Delete client/node_modules directory

--- client/node_modules/mdb-vue-ui-kit/src/composables/free/useMotionReduced.ts
+++
@@ -1,6 +0,0 @@
-export const useMotionReduced = (): boolean => {
-  if (typeof window !== "undefined" && window.matchMedia) {
-    return window.matchMedia("(prefers-reduced-motion: reduce)").matches;
-  }
-  return false; // default to false if not supported
-};
